7c96d61556243edecbd11e7023a4e5f3c529ddfc
Renzo Simonetto <renzo.bertero@tomra.com>
2019-02-22 14:32:47 +0000

    Use GPIO ports

--- gpio-ports.js
+++ gpio-ports.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = Object.freeze({
     SERVOS : {
-        LEFT : [17, 22],
-        RIGHT : [23, 24]
+        LEFT : [{value: 17}, {value: 22}],
+        RIGHT : [{value: 23}, {value: 24}]
     },
     RANGE_SENSOR : {
-        TRIG : 23,
-        ECHO : 24
+        TRIG : {value: 23},
+        ECHO : {value: 24}
     }
 });
--- rover-server.js
+++ rover-server.js
@@ -1,8 +1,11 @@
 'use strict'
-const constants = require('./gpio-ports');
+const ports = require('./gpio-ports');
+const Gpio = require('onoff').Gpio;
 const MIN_DISTANCE = 10;
 const POLLING_INTERVAL = 70;
 const SOUND_SPEED = 17150;
+
+let distance = 0;
 
 module.exports = class RoverServer {
     onMove(control) {
@@ -13,34 +16,67 @@ module.exports = class RoverServer {
     }
 
     pollDistance() {
-        this.distance = 8;
+        let pulseStart = 0;
+        let pulseEnd = 0;
+        const trigGpio = ports.RANGE_SENSOR.TRIG.gpio;
+        const echoGpio = ports.RANGE_SENSOR.ECHO.gpio;
 
-        if (this.control) {
-            const goingFwd = this.control.left.direction < 0 || this.control.right.direction;
-
-            if (this.distance < MIN_DISTANCE && goingFwd) {
-                console.log('stop!!!');
-            }
+        if (!trigGpio) {
+            ports.RANGE_SENSOR.TRIG.gpio = new Gpio(ports.RANGE_SENSOR.TRIG.value, 'out');
         }
+
+        if (!echoGpio) {
+            ports.RANGE_SENSOR.ECHO.gpio = new Gpio(ports.RANGE_SENSOR.ECHO.value, 'in');
+        }
+
+        ports.RANGE_SENSOR.TRIG.gpio.writeSync(1);
+
+        setTimeout(() => {
+            ports.RANGE_SENSOR.TRIG.gpio.writeSync(0);
+
+            while (ports.RANGE_SENSOR.ECHO.gpio.readSync() === 0) {
+                pulseStart = + new Date();
+            }
+
+            while (ports.RANGE_SENSOR.ECHO.gpio.readSync() === 1) {
+                pulseEnd = + new Date();
+            }
+
+            const pulseDuration = pulseEnd - pulseStart;
+            distance = pulseDuration * SOUND_SPEED;
+
+            if (this.control) {
+                const goingFwd = this.control.left.direction < 0 || this.control.right.direction;
+
+                if (distance < MIN_DISTANCE && goingFwd) {
+                    move('left', -1);
+                    move('right', -1);
+                }
+            }
+        }, 0,01);
     }
 
     eBrake(direction) {
-        return this.distance < MIN_DISTANCE && direction < 0;
+        return distance < MIN_DISTANCE && direction < 0;
     }
 
     move(side, direction) {
-        let ports = constants.SERVOS[side];
-        let multiplier = 1;
+        let ports = ports.SERVOS[side];
 
-        ports.forEach(port => {
-            let value = !this.eBrake(direction) && multiplier * direction < 0;
-            multiplier = multiplier * -1;
-            console.log(side, port, value);
+        ports.forEach((port, i) => {
+            if (!port.gpio) {
+                port.gpio = new Gpio(port.value, 'out');
+            }
+            let value = !this.eBrake(direction) &&
+                        direction !== 0 &&
+                        !((direction > 0 || !!i) && !(direction > 0 && !!i));
+
+            port.gpio.writeSync(+value);
+            console.log(side, port.value, value);
         });
     }
 
     constructor() {
-        this.distance = 0;
         this.control = null
         setInterval(this.pollDistance, POLLING_INTERVAL);
     }
