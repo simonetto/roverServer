95ec77e15ee8a54278f77388e282b43eabcc7067
Renzo Simonetto <renzo.bertero@tomra.com>
2019-02-22 19:41:04 +0000

    Update ports

--- gpio-ports.js
+++ gpio-ports.js
@@ -3,11 +3,11 @@ const Gpio = require('onoff').Gpio;
 
 module.exports = Object.freeze({
     SERVOS : {
-        LEFT : [new Gpio(17, 'out'), new Gpio(22, 'out')],
-        RIGHT : [new Gpio(23, 'out'), new Gpio(24, 'out')]
+        LEFT : [new Gpio(7, 'out'), new Gpio(11, 'out')],
+        RIGHT : [new Gpio(13, 'out'), new Gpio(15, 'out')]
     },
     RANGE_SENSOR : {
-        TRIG : new Gpio(23, 'out'),
+        TRIG : new Gpio(37, 'out'),
         ECHO : new Gpio(24, 'in')
     }
 });
