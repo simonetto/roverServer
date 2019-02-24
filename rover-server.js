d2642c8a1bb4190e6ce5d5e267ec7ea1aa3c3f6e
Renzo Simonetto <renzo.bertero@tomra.com>
2019-02-24 20:21:37 +0000

    Change sound sensor polling implementation

--- rover-server.js
+++ rover-server.js
@@ -3,7 +3,8 @@ const ports = require('./gpio-ports');
 
 const MAX_DISTANCE = 40;
 const POLLING_INTERVAL = 500;
-const SOUND_SPEED = 3430;
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+const SOUND_SPEED = 1e6/34321;
 
 let distance = Number.MAX_SAFE_INTEGER;
 let control = null;
@@ -17,40 +18,33 @@ module.exports = class RoverServer {
     }
 
     pollDistance() {
-        let pulseStart = 0;
-        let pulseEnd = 0;
+      let startTick;
 
-        ports.RANGE_SENSOR.TRIG.writeSync(1);
+      ports.RANGE_SENSOR.ECHO.on('alert', (level, tick) => {
+        if (level == 1) {
+          startTick = tick;
+        } else {
+          const endTick = tick;
+          const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+          distance = diff / 2 / SOUND_SPEED;
+          console.log( distance );
 
-        setTimeout(() => {
-            ports.RANGE_SENSOR.TRIG.writeSync(0);
+          if (control) {
+              const goingFwd = control.left.direction > 0 || control.right.direction > 0;
+              console.log('fwd', goingFwd);
+              console.log('distance', distance);
+              console.log('max', MAX_DISTANCE)
 
-            while (ports.RANGE_SENSOR.ECHO.readSync() === 0) {
-                pulseStart = + new Date();
-            }
-
-            while (ports.RANGE_SENSOR.ECHO.readSync() === 1) {
-                pulseEnd = + new Date();
-            }
-
-            const pulseDuration = pulseEnd - pulseStart;
-            distance = pulseDuration * SOUND_SPEED / (2 * 1000);
-
-            if (control) {
-                const goingFwd = control.left.direction > 0 || control.right.direction > 0;
-                console.log('fwd', goingFwd);
-                console.log('distance', distance);
-                console.log('max', MAX_DISTANCE)
-
-                if (distance > MAX_DISTANCE && goingFwd) {
-                    ports.SERVOS.LEFT[0].writeSync(0);
-                    ports.SERVOS.LEFT[1].writeSync(0);
-                    ports.SERVOS.RIGHT[0].writeSync(0);
-                    ports.SERVOS.RIGHT[1].writeSync(0);
-                    console.log('BREAK!')
-                }
-            }
-        }, 0.01);
+              if (distance > MAX_DISTANCE && goingFwd) {
+                  ports.SERVOS.LEFT[0].writeSync(0);
+                  ports.SERVOS.LEFT[1].writeSync(0);
+                  ports.SERVOS.RIGHT[0].writeSync(0);
+                  ports.SERVOS.RIGHT[1].writeSync(0);
+                  console.log('BREAK!')
+              }
+          }
+        }
+      });
     }
 
     eBrake(direction) {
@@ -70,6 +64,11 @@ module.exports = class RoverServer {
     }
 
     constructor() {
-        setInterval(this.pollDistance, POLLING_INTERVAL);
+        ports.RANGE_SENSOR.TRIG.digitalWrite(0);
+        setInterval(() => {
+          ports.RANGE_SENSOR.TRIG.trigger(10, 1); // Set trigger high for 10 microseconds
+        }, POLLING_INTERVAL);
+
+        this.pollDistance();
     }
 };
